346237db6e694dd8c13000eb5a17260e5b7383f0
yogi yulianto <yogi.yulianto@students.amikom.ac.id>
2019-07-25 15:07:09 +0700

    edit edit-recipe

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @IonicPage()
@@ -11,11 +12,25 @@ export class EditRecipePage implements OnInit{
 
   selectOptions = ['Easy','Medium','Hard'];
   mode = "New";
+  recipeForm: FormGroup;
 
   constructor (private navParams: NavParams) {}
 
   ngOnInit(){
     this.mode = this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  onSubmit(){
+    console.log(this.recipeForm);
+  }
+
+  private initializeForm(){
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
   }
 
 }
